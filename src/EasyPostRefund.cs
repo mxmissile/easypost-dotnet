4b65415ab3eaf0eb8718d338ddeecec7a69d556c
benmorris44 <bejmorri@gmail.com>
2013-06-07 17:31:44 -0300

    Create EasyPostRefund.cs

---
+++ src/EasyPostRefund.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Easypost
+{
+    public class EasyPostRefund : IEncodable
+    {
+        public string Carrier { get; set; }
+        public IEnumerable<string> TrackingCodes { get; set; }
+
+        public FormUrlEncodedContent AsFormUrlEncodedContent()
+        {
+            var collection = new CollectionBuilder()
+                .AddRequired("refund[carrier]".ToKvp(Carrier))
+                .AddRequired("refund[tracking_codes]".ToKvp(string.Join(",",TrackingCodes)));
+
+            return collection.AsFormUrlEncodedContent();
+        }
+    }
+}
