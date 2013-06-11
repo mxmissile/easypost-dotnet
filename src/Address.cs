35c009fb5daf4d57a8f344f87a5e2d947b7ef2ec
benmorris44 <bejmorri@gmail.com>
2013-06-11 15:26:34 -0400

    Added phone property
    The phone property is required in order for Express mail under V1 Api

--- src/Address.cs
+++ src/Address.cs
@@ -10,6 +10,7 @@ namespace Easypost
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
+        public string Phone { get; set; }
 
         public FormUrlEncodedContent AsFormUrlEncodedContent()
         {
