572272d84aa077faffad073da5cff87e692c7395
Alden Quimby <aldenquimby@gmail.com>
2013-08-21 11:47:16 -0700

    update httpmethod because v2 api isnt just all POST

--- Easypost/EasyPostClient.cs
+++ Easypost/EasyPostClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -10,10 +11,12 @@ namespace Easypost
     {
         Address CreateAddress(Address model);
         Address GetAddress(string addressId);
+        List<Address> GetAddresses();
         VerifyAddressResponse VerifyAddress(string addressId);
         
         Parcel CreateParcel(Parcel model);
         Parcel GetParcel(string parcelId);
+        List<Parcel> GetParcels();
         
         Shipment CreateShipment(Shipment model);
         Shipment GetShipment(string shipmentId);
@@ -22,42 +25,23 @@ namespace Easypost
         
         CustomsItem CreateCustomsItem(CustomsItem model);
         CustomsItem GetCustomsItem(string customsItemId);
+        List<CustomsItem> GetCustomsItems();
+        
         CustomsInfo CreateCustomsInfo(CustomsInfo model);
         CustomsInfo GetCustomsInfo(string customsInfoId);
+        List<CustomsInfo> GetCustomsInfos();
         
         ScanForm CreateScanForm(ScanForm model);
         ScanForm GetScanForm(string scanFormId);
+        List<ScanForm> GetScanForms();
         
         Refund CreateRefund(RefundRequest model);
         Refund GetRefund(string refundId);
+        List<Refund> GetRefunds();
     }
 
     public class EasyPostClient : IEasyPostClient
     {
-        private static class EasyPostUrls
-        {
-            public const string BASE_URL = "https://api.easypost.com/v2/";
-            public const string ADDRESSES = "/addresses";
-            public const string ADDRESS = "/addresses/{0}";
-            public const string ADDRESS_VERIFY = "/addresses/{0}/verify";
-            public const string PARCELS = "/parcels";
-            public const string PARCEL = "/parcels/{0}";
-            public const string SHIPMENTS = "/shipments";
-            public const string SHIPMENT = "/shipments/{0}";
-            public const string SHIPMENT_RATES = "/shipments/{0}/rates";
-            public const string SHIPMENT_BUY = "/shipments/{0}/buy";
-            public const string CUSTOM_ITEMS = "/customs_items";
-            public const string CUSTOM_ITEM = "/customs_items/{0}";
-            public const string CUSTOM_INFOS = "/customs_infos";
-            public const string CUSTOM_INFO = "/customs_infos/{0}";
-            public const string REFUNDS = "/refunds";
-            public const string REFUND = "/refunds/{0}";
-            public const string BATCHES = "/batches";
-            public const string BATCH = "/batches/{0}";
-            public const string SCAN_FORMS = "/scan_forms";
-            public const string SCAN_FORM = "/scan_forms/{0}";
-        }
-
         private readonly HttpClient _client;
 
         public EasyPostClient(string apiKey)
@@ -85,13 +69,18 @@ namespace Easypost
 
         public Address CreateAddress(Address model)
         {
-            return Execute<Address, Address>(model, EasyPostUrls.ADDRESSES);
+            return Execute<Address>(model, EasyPostUrls.ADDRESSES);
         }
 
         public Address GetAddress(string addressId)
         {
             var url = string.Format(EasyPostUrls.ADDRESS, addressId);
             return Execute<Address>(url);
+        }
+
+        public List<Address> GetAddresses()
+        {
+            return Execute<List<Address>>(EasyPostUrls.ADDRESSES);
         }
 
         public VerifyAddressResponse VerifyAddress(string addressId)
@@ -102,7 +91,7 @@ namespace Easypost
 
         public Parcel CreateParcel(Parcel model)
         {
-            return Execute<Parcel, Parcel>(model, EasyPostUrls.PARCELS);
+            return Execute<Parcel>(model, EasyPostUrls.PARCELS);
         }
 
         public Parcel GetParcel(string parcelId)
@@ -111,9 +100,14 @@ namespace Easypost
             return Execute<Parcel>(url);
         }
 
+        public List<Parcel> GetParcels()
+        {
+            return Execute<List<Parcel>>(EasyPostUrls.PARCELS);
+        } 
+
         public Shipment CreateShipment(Shipment model)
         {
-            return Execute<Shipment, Shipment>(model, EasyPostUrls.SHIPMENTS);
+            return Execute<Shipment>(model, EasyPostUrls.SHIPMENTS);
         }
 
         public Shipment GetShipment(string shipmentId)
@@ -131,12 +125,12 @@ namespace Easypost
         public BuyPostageLabelResponse BuyPostageLabel(string shipmentId)
         {
             var url = string.Format(EasyPostUrls.SHIPMENT_BUY, shipmentId);
-            return Execute<BuyPostageLabelResponse>(url);
+            return Execute<BuyPostageLabelResponse>(url, HttpMethod.Post);
         }
 
         public CustomsItem CreateCustomsItem(CustomsItem model)
         {
-            return Execute<CustomsItem, CustomsItem>(model, EasyPostUrls.CUSTOM_ITEMS);
+            return Execute<CustomsItem>(model, EasyPostUrls.CUSTOM_ITEMS);
         }
 
         public CustomsItem GetCustomsItem(string customsItemId)
@@ -145,9 +139,14 @@ namespace Easypost
             return Execute<CustomsItem>(url);
         }
 
+        public List<CustomsItem> GetCustomsItems()
+        {
+            return Execute<List<CustomsItem>>(EasyPostUrls.CUSTOM_ITEMS);
+        } 
+
         public CustomsInfo CreateCustomsInfo(CustomsInfo model)
         {
-            return Execute<CustomsInfo, CustomsInfo>(model, EasyPostUrls.CUSTOM_INFOS);
+            return Execute<CustomsInfo>(model, EasyPostUrls.CUSTOM_INFOS);
         }
 
         public CustomsInfo GetCustomsInfo(string customsInfoId)
@@ -156,9 +155,14 @@ namespace Easypost
             return Execute<CustomsInfo>(url);
         }
 
+        public List<CustomsInfo> GetCustomsInfos()
+        {
+            return Execute<List<CustomsInfo>>(EasyPostUrls.CUSTOM_INFOS);
+        } 
+
         public ScanForm CreateScanForm(ScanForm model)
         {
-            return Execute<ScanForm, ScanForm>(model, EasyPostUrls.SCAN_FORMS);
+            return Execute<ScanForm>(model, EasyPostUrls.SCAN_FORMS);
         }
 
         public ScanForm GetScanForm(string scanFormId)
@@ -167,9 +171,14 @@ namespace Easypost
             return Execute<ScanForm>(url);
         }
 
+        public List<ScanForm> GetScanForms()
+        {
+            return Execute<List<ScanForm>>(EasyPostUrls.SCAN_FORMS);
+        } 
+
         public Refund CreateRefund(RefundRequest model)
         {
-            return Execute<Refund, RefundRequest>(model, EasyPostUrls.REFUNDS);
+            return Execute<Refund>(model, EasyPostUrls.REFUNDS);
         }
 
         public Refund GetRefund(string refundId)
@@ -178,21 +187,55 @@ namespace Easypost
             return Execute<Refund>(url);
         }
 
-        private T Execute<T, TR>(TR model, string apiUri) where TR : IEncodable
+        public List<Refund> GetRefunds()
+        {
+            return Execute<List<Refund>>(EasyPostUrls.REFUNDS);
+        } 
+
+        #region helpers
+
+        private T Execute<T>(IEncodable model, string apiUri)
         {
             var content = model.AsFormUrlEncodedContent();
-            var httpResponseMessage = _client.PostAsync(apiUri, content).Result;
-            httpResponseMessage.EnsureSuccessStatusCode();
-            var response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            var responseMessage = _client.PostAsync(apiUri, content).Result;
+            responseMessage.EnsureSuccessStatusCode();
+            var response = responseMessage.Content.ReadAsStringAsync().Result;
             return JsonConvert.DeserializeObject<T>(response);
         }
 
-        private T Execute<T>(string apiUri)
+        private T Execute<T>(string apiUri, HttpMethod method = null)
         {
-            var httpResponseMessage = _client.PostAsync(apiUri, null).Result;
-            httpResponseMessage.EnsureSuccessStatusCode();
-            var response = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            var requestMessage = new HttpRequestMessage(method ?? HttpMethod.Get, apiUri);
+            var responseMessage = _client.SendAsync(requestMessage).Result;
+            responseMessage.EnsureSuccessStatusCode();
+            var response = responseMessage.Content.ReadAsStringAsync().Result;
             return JsonConvert.DeserializeObject<T>(response);
         }
+
+        private static class EasyPostUrls
+        {
+            public const string BASE_URL = "https://api.easypost.com/v2/";
+            public const string ADDRESSES = "addresses";
+            public const string ADDRESS = "addresses/{0}";
+            public const string ADDRESS_VERIFY = "addresses/{0}/verify";
+            public const string PARCELS = "parcels";
+            public const string PARCEL = "parcels/{0}";
+            public const string SHIPMENTS = "shipments";
+            public const string SHIPMENT = "shipments/{0}";
+            public const string SHIPMENT_RATES = "shipments/{0}/rates";
+            public const string SHIPMENT_BUY = "shipments/{0}/buy";
+            public const string CUSTOM_ITEMS = "customs_items";
+            public const string CUSTOM_ITEM = "customs_items/{0}";
+            public const string CUSTOM_INFOS = "customs_infos";
+            public const string CUSTOM_INFO = "customs_infos/{0}";
+            public const string REFUNDS = "refunds";
+            public const string REFUND = "refunds/{0}";
+            public const string BATCHES = "batches";
+            public const string BATCH = "batches/{0}";
+            public const string SCAN_FORMS = "scan_forms";
+            public const string SCAN_FORM = "scan_forms/{0}";
+        }
+
+        #endregion
     }
 }
--- Easypost/VerifyAddressResponse.cs
+++ Easypost/VerifyAddressResponse.cs
@@ -4,5 +4,10 @@ namespace Easypost
     {
         public Address Address { get; set; }
         public string Message { get; set; }
+
+        public bool IsMissingInfo
+        {
+            get { return Address != null && !string.IsNullOrEmpty(Message); }
+        }
     }
 }
